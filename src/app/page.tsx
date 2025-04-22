fa9e7ac06bf3ada2cd79d4fffd95a6f0911eed3d
DootDaMoop <jasonkhotsombath@gmail.com>
2025-04-22 01:18:28 -0400

    Basic NextAuth Working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,11 @@ import '@fontsource/sansita';
 import SignInBanner from "@/components/ui/sign-in-banner";
 import HomePage from "@/pages/homePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegistrationButton from "@/components/ui/registrationButton";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
 <>
@@ -41,6 +43,7 @@ export default async function Home() {
         fontFamily: "'Sansita', sans-serif"}}>
         Looks like you're not signed in. Click the sign in button below to view our sign in options!
       </p>
+      <RegistrationButton />
     </div>
   )}
 </div>
@@ -49,15 +52,15 @@ export default async function Home() {
   <div style={{ width: "100%" }}>
     <AuthButton />
   </div>
-          {/* Button to navigate to HomePage */}
-          <div style={{ marginTop: "20px" }}>
-            <Link href="/homePage">
-              <button style={{ padding: "10px 20px", fontSize: "16px" }}>
-                Go to Home Page
-              </button>
-            </Link>
-          </div>
-</div>
+    {/* Button to navigate to HomePage */}
+    <div style={{ marginTop: "20px" }}>
+      <Link href="/homePage">
+        <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+          Go to Home Page
+        </button>
+      </Link>
+    </div>
+  </div>
 </div>
 </>
 );
