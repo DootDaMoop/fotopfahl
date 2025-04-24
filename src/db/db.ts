1d292a2bde376575efbee78df206f9c8f32eb666
DootDaMoop <jasonkhotsombath@gmail.com>
2025-04-24 03:00:53 -0400

    Migration Edits

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,18 @@
 import pg from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 import { Database } from './types.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const dialect = new PostgresDialect({
     pool: new pg.Pool({
-    database: 'test',
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    port: 5432,
-    max: 10,
+    database: process.env.DATABASE_NAME || 'test',
+    host: process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DATABASE_USER || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
+    port: Number(process.env.DATABASE_PORT) || 5432,
+    max: Number(process.env.POOL_MAX) || 10,
     })
 })
 
--- src/db/types.ts
+++ src/db/types.ts
@@ -1,14 +1,36 @@
-import { ColumnType, Generated, PostgresAdapter } from "kysely";
+import { Generated } from "kysely";
 
 export type Database = {
-    posts: Post;
+    user: UserTable;
+    post: PostTable;
+    comment: CommentTable;
 };
 
-export interface Post {
+
+export interface UserTable {
     id: Generated<number>;
+    profilePicture: string | null;
+    userName: string;
+    email: string; 
+    password: string | null;
+    name: string | null;
+    provider: string | null; 
+}
+
+export interface PostTable {
+    id: Generated<number>;
+    userId: number;
     title: string; 
     description: string | null; //description is optional. can be filled with img data if dataPermission = true
     image: string; 
-    mapData: string; 
-    dataPermission: boolean; 
+    mapData: any; 
+    dataPermission: boolean;
+    likes: number; 
+}
+
+export interface CommentTable {
+    id: Generated<number>;
+    postId: number; 
+    userId: number; 
+    content: string; 
 }
